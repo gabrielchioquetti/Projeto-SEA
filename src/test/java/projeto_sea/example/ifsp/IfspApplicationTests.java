6276110caae0682bc9516dbbb81b627031e04db2
gabrielchioquetti <gabrielchioquetti@gmail.com>
2026-04-02 15:39:10 -0300

    Configuração do Spring Boot, controller inicial e página de login com HTML/CSS

---
+++ src/test/java/projeto_sea/example/ifsp/IfspApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto_sea.example.ifsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
