295d537061ef78bdae0dc95e34ad434df1b690c3
scrim-froid <moraez319@gmail.com>
2026-06-09 13:58:24 -0300

    resolvendo conflitos

--- src/main/java/Projeto_SEA/IFSP/Repository/ProfessorRepository.java
+++ src/main/java/Projeto_SEA/IFSP/Repository/ProfessorRepository.java
@@ -1,28 +1,17 @@
 package Projeto_SEA.IFSP.Repository;
 
-<<<<<<< HEAD
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-=======
-import java.util.List;
-
->>>>>>> 2b6f2445d58bf2d1a27559a6f6321a82de3ff9df
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import Projeto_SEA.IFSP.Model.Professor;
 
 @Repository
 public interface ProfessorRepository extends JpaRepository<Professor, Long> {
-<<<<<<< HEAD
 
     Page<Professor> findByNomeContainingIgnoreCase(
         String nome,
         Pageable pageable
 );
 
-=======
-    List<Professor> findByNomeContainingIgnoreCase(
-        String nome
-    );
->>>>>>> 2b6f2445d58bf2d1a27559a6f6321a82de3ff9df
 }
--- src/main/java/Projeto_SEA/IFSP/Repository/TurmaRepository.java
+++ src/main/java/Projeto_SEA/IFSP/Repository/TurmaRepository.java
@@ -1,12 +1,7 @@
 package Projeto_SEA.IFSP.Repository;
 
-<<<<<<< HEAD
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-=======
-import java.util.List;
-
->>>>>>> 2b6f2445d58bf2d1a27559a6f6321a82de3ff9df
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,11 +9,5 @@ import Projeto_SEA.IFSP.Model.Turma;
 
 @Repository
 public interface TurmaRepository extends JpaRepository<Turma, Long>{
-<<<<<<< HEAD
     Page<Turma> findByNomeContainingIgnoreCase(String nome, Pageable pageable);
-=======
-    List<Turma> findByNomeContainingIgnoreCase(
-        String nome
-    );
->>>>>>> 2b6f2445d58bf2d1a27559a6f6321a82de3ff9df
 }
